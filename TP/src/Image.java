fa5d288cdc1875050a94e5024ed7e00221c7187b
QSL <q.s.l@live.fr>
2012-09-21 12:07:32 +0200

    Fin

--- TP/src/Image.java
+++ TP/src/Image.java
@@ -3,12 +3,11 @@ import java.util.Vector;
 
 public class Image {
 
-	
+	public String intro;
+	public String comment;
 	public int largeur;
 	public int hauteur;
 	public int niveauMax;
-	
-	public Vector<Integer> binaire;
 	
 	
 	
